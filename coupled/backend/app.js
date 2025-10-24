36154df694cef4ba76cb9e6c6387e091d8689e6b
Sergio Acosta <sergioacostaquintana@gmail.com>
2025-10-24 23:22:18 +0100

    feat(api): 🚀 Update Swagger integration to serve OpenAPI spec
    * Removed `swagger-ui-express` dependency.
    * Added endpoint `/openapi.json` to serve the OpenAPI specification.
    * Improved API documentation accessibility.

--- coupled/backend/app.js
+++ coupled/backend/app.js
@@ -8,7 +8,6 @@ import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from 'path';
 import swaggerJsdoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
 import { fileURLToPath } from 'url';
 import { v4 as uuidv4 } from "uuid";
 
@@ -62,8 +61,14 @@ const swaggerOptions = {
     apis: [__filename],
 };
 
+
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/openapi.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+});
+
 
 /**
  * @swagger
